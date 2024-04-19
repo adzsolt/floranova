a62729c2472636fe64c2dce6b62b20f93ed3b1f2
Adorjani Zsolt <zsolt.adorjani@gosocialdev.eu>
2024-04-19 21:51:12 +0300

    views

---
+++ resources/views/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Floranova production</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+    <!-- Scripts -->
+    <script type="module">
+        import RefreshRuntime from 'http://localhost:5173/@react-refresh'
+        RefreshRuntime.injectIntoGlobalHook(window)
+        window.$RefreshReg$ = () => {}
+        window.$RefreshSig$ = () => (type) => type
+        window.__vite_plugin_react_preamble_installed__ = true
+    </script>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/coreui/src/index.jsx'])
+
+    <!-- Styles -->
+</head>
+<body>
+<div id="root"></div>
+
+</body>
+</html>
+
