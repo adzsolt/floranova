f33b77727543a66e13722f161bea1667f30663a0
Adorjani Zsolt <zsolt.adorjani@gosocialdev.eu>
2024-04-23 10:17:27 +0300

    price decimal

---
+++ public/build/assets/Users-tOYogVFm.js
@@ -0,0 +1,234 @@
+import { r as reactExports, j as jsxRuntimeExports } from "./client-DnTwIhck.js";
+import { a as CRow, b as CCol, d as CCard, a0 as CCardHeader, e as CCardBody, aP as CSmartTable, J as CAvatar, s as CBadge, k as CButton, aj as CCollapse, aF as CModal, aG as CModalHeader, aH as CModalTitle, aI as CModalBody, aJ as CModalFooter, ar as CLoadingButton, aC as CAlert, ac as CCardTitle, i as CIcon } from "./index.es-DQOCfkkb.js";
+import "./ProBadge-Trb8gTuP.js";
+import { g as useNavigate, G as GlobalContext } from "./index-FVFFpgU8.js";
+import { a as axios } from "./axios-fEUzBR8L.js";
+import { c as cilUserPlus } from "./cil-user-plus-kfZGZ2sS.js";
+import "./cil-user-q2NU2AQZ.js";
+const Tables = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = reactExports.useState(false);
+  const [condemnedId, setCondemnedId] = reactExports.useState(null);
+  const controller = new AbortController();
+  const { users, setUsers } = reactExports.useContext(GlobalContext);
+  const [error, setError] = reactExports.useState("");
+  const [details, setDetails] = reactExports.useState([]);
+  const columns = [
+    {
+      key: "avatar",
+      label: "",
+      filter: false,
+      sorter: false
+    },
+    {
+      label: "Név",
+      key: "name",
+      _style: { width: "20%" }
+    },
+    {
+      label: "Regisztráció",
+      key: "registered",
+      _style: { width: "20%" }
+    },
+    {
+      label: "Jog",
+      key: "role",
+      _style: { width: "20%" }
+    },
+    {
+      label: "Státus",
+      key: "status",
+      _style: { width: "20%" }
+    },
+    {
+      key: "show_details",
+      label: "",
+      filter: false,
+      sorter: false
+    }
+  ];
+  const getList = () => {
+    setIsLoading(true);
+    setError("");
+    axios.get(
+      "/get-users",
+      {
+        signal: controller.signal
+      }
+    ).then((response) => {
+      setUsers(response.data.users);
+    }).catch((error2) => {
+      console.log("ERROR:: ", error2);
+      setError(error2.message);
+    }).finally(() => {
+      setIsLoading(false);
+    });
+  };
+  reactExports.useEffect(() => {
+    getList();
+    return () => {
+      controller.abort();
+    };
+  }, []);
+  const getBadge = (status) => {
+    switch (status) {
+      case "Active":
+        return "success";
+      case "Inactive":
+        return "secondary";
+      case "Pending":
+        return "warning";
+      case "Banned":
+        return "danger";
+      default:
+        return "primary";
+    }
+  };
+  const toggleDetails = (index) => {
+    const position = details.indexOf(index);
+    let newDetails = details.slice();
+    if (position !== -1) {
+      newDetails.splice(position, 1);
+    } else {
+      newDetails = [...details, index];
+    }
+    setDetails(newDetails);
+  };
+  const condemnId = (e) => {
+    setCondemnedId(e.currentTarget.getAttribute("data-id"));
+  };
+  const spareId = () => {
+    setCondemnedId(null);
+  };
+  const handleEdit = (e) => {
+    const id = e.currentTarget.getAttribute("data-id");
+    navigate(`/users/edit/${id}`);
+  };
+  const handleDelete = () => {
+    setIsLoading(true);
+    axios.post(
+      "/delete-user",
+      {
+        id: condemnedId
+      }
+    ).then((response) => {
+      setCondemnedId(null);
+      getList();
+    }).catch((error2) => {
+      console.log("ERROR:: ", error2);
+    }).finally(() => {
+      setIsLoading(false);
+    });
+  };
+  return /* @__PURE__ */ jsxRuntimeExports.jsxs(CRow, { children: [
+    /* @__PURE__ */ jsxRuntimeExports.jsx(CCol, { xs: 12, children: /* @__PURE__ */ jsxRuntimeExports.jsxs(CCard, { className: "mb-4", children: [
+      /* @__PURE__ */ jsxRuntimeExports.jsxs(CCardHeader, { children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsx("strong", { children: "Felhasználói lista" }),
+        " ",
+        /* @__PURE__ */ jsxRuntimeExports.jsx("small", {})
+      ] }),
+      /* @__PURE__ */ jsxRuntimeExports.jsxs(CCardBody, { children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsx(
+          CSmartTable,
+          {
+            items: users,
+            activePage: 1,
+            cleaner: true,
+            clickableRows: true,
+            columns,
+            columnFilter: true,
+            columnSorter: true,
+            footer: true,
+            itemsPerPageSelect: true,
+            itemsPerPage: 5,
+            pagination: true,
+            onFilteredItemsChange: (items) => {
+            },
+            onSelectedItemsChange: (items) => {
+            },
+            scopedColumns: {
+              avatar: (item) => /* @__PURE__ */ jsxRuntimeExports.jsx("td", { children: /* @__PURE__ */ jsxRuntimeExports.jsx(CAvatar, { src: `/images/avatars/${item.avatar}` }) }),
+              status: (item) => /* @__PURE__ */ jsxRuntimeExports.jsx("td", { children: /* @__PURE__ */ jsxRuntimeExports.jsx(CBadge, { color: getBadge(item.status), children: item.status }) }),
+              show_details: (item) => {
+                return /* @__PURE__ */ jsxRuntimeExports.jsx("td", { className: "py-2", children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+                  CButton,
+                  {
+                    color: "primary",
+                    variant: "outline",
+                    shape: "square",
+                    size: "sm",
+                    onClick: () => {
+                      toggleDetails(item.id);
+                    },
+                    children: details.includes(item.id) ? "Elrejt" : "Mutat"
+                  }
+                ) });
+              },
+              details: (item) => {
+                return /* @__PURE__ */ jsxRuntimeExports.jsx(CCollapse, { visible: details.includes(item.id), children: /* @__PURE__ */ jsxRuntimeExports.jsxs(CCardBody, { className: "p-3", children: [
+                  /* @__PURE__ */ jsxRuntimeExports.jsx("h4", { children: item.username }),
+                  /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { className: "text-muted", children: [
+                    "Felhasználó  ",
+                    item.registered,
+                    " óta"
+                  ] }),
+                  /* @__PURE__ */ jsxRuntimeExports.jsx(CButton, { size: "sm", color: "info", "data-id": item.id, onClick: handleEdit, children: "Felhasználó szerkesztése" }),
+                  /* @__PURE__ */ jsxRuntimeExports.jsx(CButton, { size: "sm", color: "danger", className: "ml-1", "data-id": item.id, variant: "outline", onClick: condemnId, children: "Törlés" })
+                ] }) });
+              }
+            },
+            selectable: true,
+            sorterValue: { column: "status", state: "asc" },
+            tableFilter: true,
+            tableProps: {
+              className: "add-this-class",
+              responsive: true,
+              striped: true,
+              hover: true
+            },
+            tableBodyProps: {
+              className: "align-middle"
+            }
+          }
+        ),
+        /* @__PURE__ */ jsxRuntimeExports.jsxs(CModal, { visible: condemnedId !== null, onClose: spareId, children: [
+          /* @__PURE__ */ jsxRuntimeExports.jsx(CModalHeader, { onClose: () => setVisible(false), children: /* @__PURE__ */ jsxRuntimeExports.jsx(CModalTitle, { children: "Delete user" }) }),
+          /* @__PURE__ */ jsxRuntimeExports.jsxs(CModalBody, { children: [
+            "Are you sure you want to delete user #",
+            condemnedId,
+            "?"
+          ] }),
+          /* @__PURE__ */ jsxRuntimeExports.jsxs(CModalFooter, { children: [
+            /* @__PURE__ */ jsxRuntimeExports.jsx(CButton, { color: "secondary", onClick: spareId, children: "Close" }),
+            /* @__PURE__ */ jsxRuntimeExports.jsx(CLoadingButton, { color: "danger", onClick: handleDelete, disabled: isLoading, loading: isLoading, children: "Delete User" })
+          ] })
+        ] })
+      ] })
+    ] }) }),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(CCol, { xs: 12, children: /* @__PURE__ */ jsxRuntimeExports.jsxs(CAlert, { color: "danger", className: "d-flex justify-content-between", visible: error !== "", children: [
+      error,
+      " ",
+      /* @__PURE__ */ jsxRuntimeExports.jsx(CButton, { color: "light", onClick: () => getList(), disabled: isLoading, children: "Retry" })
+    ] }) })
+  ] });
+};
+const Users = () => {
+  reactExports.useState(true);
+  const navigate = useNavigate();
+  const handleAdd = () => {
+    navigate("/users/add");
+  };
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(CCard, { className: "mb-3", children: /* @__PURE__ */ jsxRuntimeExports.jsx(CCardBody, { children: /* @__PURE__ */ jsxRuntimeExports.jsxs(CRow, { children: [
+    /* @__PURE__ */ jsxRuntimeExports.jsxs(CCol, { className: "d-flex justify-content-between mb-3", children: [
+      /* @__PURE__ */ jsxRuntimeExports.jsx(CCardTitle, { children: "Felhasználók" }),
+      /* @__PURE__ */ jsxRuntimeExports.jsxs(CButton, { color: "light", onClick: handleAdd, children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsx(CIcon, { icon: cilUserPlus, className: "btn-icon mr-2" }),
+        " Felhszasználó hozzáadása"
+      ] })
+    ] }),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(CCol, { xs: 12, children: /* @__PURE__ */ jsxRuntimeExports.jsx(Tables, {}) })
+  ] }) }) });
+};
+export {
+  Users as default
+};
