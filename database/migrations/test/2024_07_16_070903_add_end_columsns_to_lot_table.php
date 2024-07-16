ca0713e122d80b4e209651dd5fe84c8b128873d7
Adorjani Zsolt <zsolt.adorjani@gosocialdev.eu>
2024-07-16 16:16:15 +0300

    migration_2

---
+++ database/migrations/test/2024_07_16_070903_add_end_columsns_to_lot_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lots', function (Blueprint $table) {
+            $table->integer('final_quantity')->after('quantity')->nullable();
+            $table->integer('final_price')->after('end_date')->nullable();
+            $table->integer('group_id')->after('fertilizer_volume')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lots', function (Blueprint $table) {
+            $table->dropColumn('final_quantity');
+            $table->dropColumn('final_price');
+            $table->dropColumn('group_id');
+        });
+    }
+};
