0e3caa15cb98a37ae0ec01da75f7283a0ea75fa3
Adorjani Zsolt <zsolt.adorjani@gosocialdev.eu>
2024-11-07 13:54:43 +0200

    end date modification

--- app/Http/Controllers/LotController.php
+++ app/Http/Controllers/LotController.php
@@ -185,7 +185,17 @@ class LotController extends Controller
 
         $start = $data['start_date'];
         $end = $data['end_date'];
+
+
+        $carbon_end_date = Carbon::parse($end);
+
         $lot = Lot::where('id', $id)->first();
+
+        $lot_end_date_carbon = Carbon::parse($lot->end_date);
+
+        if(isset($lot_end_date_carbon) and $lot_end_date_carbon < $carbon_end_date){
+            $end = $lot->end_date;
+        }
 
         $data['lot_name'] = $lot->name;
 
